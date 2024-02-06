14eaed8166a6c53ed5e1253634265b53c4a43567
gshor <gadschor@gmail.com>
2024-02-06 10:55:18 +0200

    User

---
+++ Telahi.DotNet.OOP/Auth/User.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection.Metadata.Ecma335;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Telahi.DotNet.OOP.Auth
+{
+	public class User
+	{
+
+		public int Id { get; set; }
+		public string UserName { get; set; }
+		public string PassWord { get; set; }
+
+
+		public User() { }
+
+		public User(string username)
+		{
+			UserName = username;
+		}
+
+		/// <summary>
+		/// Arrow Function
+		/// </summary>
+		/// <returns></returns>
+		public override string ToString() => $"{Id}:{UserName}";
+		
+		
+	}
+}
